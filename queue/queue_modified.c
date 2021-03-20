4a51fba1ef4bde3a700a759be4f405025895ed87
barbade-shivaprasad <78468138+barbade-shivaprasad@users.noreply.github.com>
2021-03-20 12:24:03 +0530

    Update queue_modified.c

--- queue/queue_modified.c
+++ queue/queue_modified.c
@@ -2,50 +2,56 @@
 struct queue
 {
     int c[5];
-    int r;
+    int rear;
+    int front;
 };
-int a(int b,int c[5])
+int rep(int front,int rear,int c[])
 {
-    if(b==4)
-    {
-        return 0;
-    }
-    else
-    {
-        c[b]=c[b+1];
-        b++;
-        return a(b,c);
-    }
+        if(front==rear)
+            return 0;
+        else
+        {
+            c[front]=c[front+1];
+        front++;
+        rep(front,rear,c);
+        }
 }
 int main()
-{   int b,d;
+{   int front,rear,b,d;
     struct queue q;
-    q.r=-1;
+    q.front=0;
+    q.rear=0;
     do{printf("Enter\n 1.Enqueue\n 2.Dequeue\n 3.Exit\n==>");
     scanf("%d",&b);
     switch(b)
     {
     case 1:
-        if(q.r==4)
+        if(q.rear==4)
             printf("Cannot add any more...\n");
         else
         {
-            q.r++;
             printf("Enter the value:");
             scanf("%d",&d);
-            q.c[q.r]=d;
-            printf("\nthe first value is %d\n\n",q.c[0]);
+            q.c[q.rear]=d;
+            q.rear++;
+            printf("\nthe first value is %d\n\n",q.c[q.front]);
+            front=q.front;
         }
         break;
     case 2:
-        if(q.r==-1)
-            printf("\nempty!!!\n");
-        else
-        {
-            a(0,q.c);
-            q.r--;
-            printf("\nthe first value is %d\n",q.c[0]);
-        }
+
+            if(q.front==q.rear)
+                printf("Queue is empty\n");
+            else
+            {
+                rep(front,q.rear,q.c);
+                q.rear--;
+                if(q.front==q.rear)
+                printf("Queue is empty");
+                else
+                printf("The front value is %d\n",q.c[0]);
+            }
+
         break;
     case 3:
         printf("Thank u");
